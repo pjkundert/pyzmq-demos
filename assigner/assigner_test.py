94d51c04484fc77872494fb7f8e9fa8e855483b5
Perry Kundert <perry@kundert.ca>
2011-09-21 07:10:09 -0600

    Initial foray into ROUTER-ROUTER request broker to assign server to client

--- assigner/assigner_test.py
+++ assigner/assigner_test.py
@@ -1,4 +1,5 @@
 import zmq
+import zhelpers
 
 port			= 55667
 
@@ -41,4 +42,51 @@ def test_closing_full():
     out.close()
     inc.close()
     ctx.term()
+
+
+def test_sending_receiver_ids():
+    """
+    When multiple sockets are connected and to the same destination, are the
+    "receiving" socket IDs the same for all sending sockets?  Say we have:
+
+        XREQ  <--\
+                  +--> XREP (B)
+    (A) XREP  <--/
+
+    if we send via an XREQ to an XREP, and we use it to send us back the ID, can
+    we use this same ID to route *outwards* via the (A) XREP, *to* the same
+    destinatino XREP (B)?  Or, is a (random) ID for XREP (B) synthesiAed by each
+    XREQ/XREP that connects to is (unless, of course, we sent an zmq.IDENTITY
+    for XREP (B), which is obsolete in 0MQ 3+...)
+    """
+    ctx			= zmq.Context()
+
+    global port
+    port	       += 1
+
+    xrepb		= ctx.socket( zmq.XREP )
+    xrepb.bind( "tcp://*:%d" % port )
+
+    xreqa		= ctx.socket( zmq.XREQ )
+    xreqa.connect( "tcp://localhost:%d" % port )
+
+    xrepa		= ctx.socket( zmq.XREP )
+    xrepa.connect( "tcp://localhost:%d" % port )
+
+    # From XREQ, XREP(B) will add a source socket ID on recv...
+    xreqa.send_multipart( [ '', 'something' ] )
+    rx			= xrepb.recv_multipart()
+    print "msg: %s" % ", ".join( [ zhelpers.format_part( msg )
+                                   for msg in rx ] )
+    assert len( rx ) == 3
+
+    # Try using that socket ID as a destination for routing to the same XREP(B)
+    # from XREP(A)
+    poller = zmq.Poller()
+    poller.register( xrepb, zmq.POLLIN)
+    xrepa.send_multipart( rx )
+    socks = dict(poller.poll( timeout=1000 ))
+    assert len( socks ) == 1
+    rx2			= xrepb.recv_multipart()
+
 
