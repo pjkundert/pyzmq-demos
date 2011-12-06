eed7a98b4c4eb465b15a48af7a7203dc3e191f13
Perry Kundert <perry@kundert.ca>
2011-12-06 12:51:16 -0700

    Clean up zmqjsonrpc docs, api

--- assigner/zmqjsonrpc.py
+++ assigner/zmqjsonrpc.py
@@ -1,43 +1,78 @@
-
 # 
-# 0MQ interface for JSON-RPC
+# pyzmq-jsonrpc.py
 # 
-# Based on the reference Python JSON-PRC implementation from http://json-rpc.org, and
-# on the 
+# A JSON-RPC Service Proxy interface using 0MQ for transport
+# 
+# Based on the reference Python JSON-PRC implementation from
+# http://json-rpc.org Presents an interface similar to (close to
+# plug-in compatible with) json-rpc.
+#
+# ServiceProxy( socket[, serviceName[, requestId )
+# 
+#     Creates a proxy for the named object (optional; default None
+#     invokes global methods).  Invoking:
+# 
+#          <proxy>.method(<params>)
+# 
+#     Sends the 0MQ 2-part message: 'content-type: application/json',
+#     followed by a JSON-RPC 2.0 encoded remote method invocation of:
+#     
+#          [serviceName.]method(<params>)
+# 
+#     The result (or exception, raised as a JSONRPCException) is
+#     returned via JSON-RPC encoding.
 # 
 
 import zmq
 import zhelpers
-import jsonrpc
 import json
 
-def RequestId( initial=1 ):
+VERSION                         = "2.0"
+CONTENT_TYPE                    = "content-type: application/json"
+JSON_OPTIONS                    = {
+    "indent":           None,           # Not pretty-printed
+    "separators":       (',', ':'),     # Most compact representation
+    "allow_nan":        True,           # Allow nan/inf to be encoded
+    }
+
+
+def request_id( initial=1 ):
     """
     JSON-RPC request 'id' enumerator generator.  By default, returns a
-    monotonically increasing integer value.  May be shared by many ServiceProxy
-    objects, but is not thread-safe.
+    monotonically increasing integer value.  May be safely shared by
+    the many ServiceProxy objects representing the methods being, but
+    is not thread-safe.
     """
     while True:
         yield initial
-        initial		       += 1
-        
+        initial                += 1
+
 
 class session_socket( object ):
+    """
+    Transparently supports session ID lists and/or 0MQ routing prefix
+    lists, while presenting the basic 0MQ send_/recv_multipart
+    interface.
+
+    Encapsulates a zmq.XREQ or zmq.XREP socket endpoint,
+    adding/stripping optional session ID and/or routing label prefix
+    lists on each send_/recv_multipart invocation.
+    """
     def __init__( self, socket, sessid, prefix=None ):
-        self.__socket 		= socket
+        self.__socket           = socket
         assert sessid is None or type( sessid ) is list
-        self.__sessid		= sessid or []
+        self.__sessid           = sessid or []
         assert prefix is None or type( prefix ) is list
-        self.__prefix		= prefix or []
+        self.__prefix           = prefix or []
 
     def send_multipart( self, data ):
         """
         Sends data list with the supplied routing prefix (if any), and
-        prepends the session id before the supplied data.
+        prepends any session id before the supplied data.
         """
-        prefix			= self.__prefix
+        prefix                  = self.__prefix
         assert type( data ) is list
-        payload			= self.__sessid + data
+        payload                 = self.__sessid + data
         print "Sending (to %s): %s" % (
             ", ".join( [ zhelpers.format_part( lbl )
                          for lbl in prefix ] ),
@@ -47,14 +82,15 @@ class session_socket( object ):
 
     def recv_multipart( self ):
         """
-        Removes any leading session id from the result; fails if no session id is present.
+        Removes any leading session id from the result; fails if no
+        session id is present.
         """
-        result			= self.__socket.recv_multipart()
+        result                  = self.__socket.recv_multipart()
         if type( result ) is tuple:
-            prefix, payload 	= result
+            prefix, payload     = result
         else:
-            prefix		= []
-            payload		= result
+            prefix              = []
+            payload             = result
         print "Receive (fr %s): %s" % (
             ", ".join( [ zhelpers.format_part( lbl )
                          for lbl in prefix ] ),
@@ -62,49 +98,94 @@ class session_socket( object ):
                          for msg in payload ] ))
         if self.__sessid:
             if result[:len(self.__sessid)] == self.__sessid:
-                result		= result[len(self.__sessid):]
+                result          = result[len(self.__sessid):]
             else:
                 raise Exception( "Expected session ID: %s" % (
                         ", ".join( self.__sessid )))
         return result
 
-class ServiceProxy( jsonrpc.ServiceProxy ):
-    """
-    Creates a persistent connection using a 0MQ socket, and allows remote
-    invocation of methods via JSON-RPC conventions.  The 0MQ socket provided is
-    returned in many ServiceProxy objects, so ensure that they are not shared
-    between threads, as 0MQ socket objects are not thread-safe.
-    """
-    def __init__(self, socket, name=None, reqid=None):
-        self.__socket		= socket
-        self.__name		= name
-        self.__reqid		= reqid if reqid is not None else RequestId()
+# 
+# The jsonrpc ServiceProxy interface, minimally modified to accept a
+# 0MQ socket, instead of a (HTTP) URL to access the service endpoint.
+# 
 
+class JSONRPCException( Exception ):
+    """
+    The Exception raised if an non-None 'error' is returned via the
+    JSON-RPC response.
+    """
+    def __init__(self, rpcError):
+        Exception.__init__(self)
+        self.error = rpcError
+
+class ServiceProxy( object ):
+    """
+    Implements remote invocation of methods via JSON-RPC conventions.
+    The 0MQ socket provided is returned in many ServiceProxy objects,
+    so ensure that they are not shared between threads, as 0MQ socket
+    objects are not thread-safe.
+
+    Expects the 0MQ socket to return only the response data (not any
+    routing prefix information); encapsulate other 0MQ socket types
+    (eg. zmq.XREP, which return (labels, messages) tuples from
+    recv_multipart()), so they handle the routing prefix labels, etc.
+
+    TODO
+    
+    Support timeouts and tx/rx of keepalive heartbeats, to ensure
+    remote service remains alive on long-lived requests.  This
+    requires handling of 0MQ timeouts on recv_multipart, and
+    transmission of appropriate serialized keepalive requests,
+    reception of keepalive responses.  The detection of no response
+    within a certain period must trigger an appropriate Exception
+    indicating failure of the service.
+
+    No multithreading is required on this side, as the keepalive
+    heartbeat may be transmitted (and detection of lack of response
+    computed) on timeout of recv_multipart.  On the remote
+    (ServiceHandler) end, however, an asynchronous thread may be
+    required to service the keepalive heartbeat responses in a timely
+    manner.
+    """
+    def __init__(self, socket, serviceName=None, requestId=None):
+        self.__socket           = socket
+        self.__name             = serviceName
+        self.__reqid            = requestId if requestId is not None else request_id()
     def __getattr__( self, name ):
         if self.__name != None:
             name = "%s.%s" % (self.__name, name)
-        return ServiceProxy( self.__socket, name=name, reqid=self.__reqid )
+        return ServiceProxy( self.__socket, serviceName=name, requestId=self.__reqid )
 
     def __call__( self, *args ):
         """
         Invoke the method via JSON-RPC, via the 0MQ socket, and return
         the result if successful.
-        """
-        postdict		={
-	       'jsonrpc':	"2.0",
-               'method': 	self.__name, 
-               'params': 	args,
-               'id':		str( self.__reqid.next() ),
-               }
-        print "RPC Request: ", repr( postdict )
-        postdata = json.dumps( postdict )
-        self.__socket.send_multipart(
-            ['content-type: application/json', postdata] )
-        respdata 		= self.__socket.recv_multipart()
-        resp = json.loads( respdata[0] )
-        if resp['error'] != None:
-            raise JSONRPCException(resp['error'])
 
+        All arguments are encoded and passed as Unicode, as per the
+        JSON-RPC spec.  The request id is 
+        """
+        method                  = unicode( self.__name )
+        reqid                   = unicode( self.__reqid.next() )
+        data                    = json.dumps( {
+                                      "jsonrpc":        VERSION,
+                                      "method":         method,
+                                      "params":         args,
+                                      "id":             reqid,
+                                      }, **JSON_OPTIONS )
+        self.__socket.send_multipart( [CONTENT_TYPE, data] )
+
+        # Receive and decode JSON response data.  May raise
+        # ValueError, etc. exception, if not valid JSON.  Will raise
+        # AttributeError exceptions if response dict doesn't contain
+        # required 'error', 'result', 'id' fields, and AssertionError
+        # if values are not as expected.  See jsonrpc/proxy.py for
+        # reference implementation.
+        respdata                = self.__socket.recv_multipart()
+        resp                    = json.loads( respdata[0] )
+        assert resp['id'] == reqid, \
+            "Invalid JSON-RPC id '%s'; expected '%s'"  % ( resp['id'], reqid )
+        if resp['error'] != None:
+            raise jsonrpc.JSONRPCException(resp['error'])
         return resp['result']
          
 
--- assigner/zmqjsonrpc_test.py
+++ assigner/zmqjsonrpc_test.py
@@ -1,48 +1,54 @@
 import zmq
-import zmqrpc
+import zmqjsonrpc
 import json
 import threading
 
 def test_zmqrpc_simulate_server():
-    clictx		= zmq.Context()
-    cli			= clictx.socket( zmq.XREQ )
+    clictx                      = zmq.Context()
+    cli                         = clictx.socket( zmq.XREQ )
     cli.connect( 'tcp://localhost:11223' )
 
-    svrctx		= zmq.Context()
-    svr			= svrctx.socket( zmq.XREP )
+    svrctx                      = zmq.Context()
+    svr                         = svrctx.socket( zmq.XREP )
     svr.bind( 'tcp://*:11223' )
 
-    # Simulate the Server side of the RPC request
+    # Simulate the Server side of the 0MQ JSON-RPC request
     def svrfun():
-        prefix, request	= svr.recv_multipart()
+        prefix, request         = svr.recv_multipart()
         assert len( prefix ) == 1
         assert len( request ) == 3
-        assert request[0].lower()	== '1' # Session ID
-        sessid	 	= request[0]
-        assert request[1].lower()	== 'content-type: application/json';
-        requestdict		= json.loads( request[2] )
-        print "Request: ", repr( requestdict )
-        replydict 	= {
-            'jsonrpc':	"2.0",
-            'index':	"1",
-            'result':	", ".join( [ str( p ) for p in requestdict['params']] ),
-            'id':	"1",
-            'error':	None,
+        assert request[0].lower() == "1"     # Session ID
+        assert request[1].lower() == zmqjsonrpc.CONTENT_TYPE
+        sessid                  = request[0]
+
+        requestdict             = json.loads( request[2] )
+        assert requestdict['method'] == "boo.foo"
+        assert len( requestdict['params'] ) == 3
+
+        result                  = ", ".join( [ str( p )
+                                               for p in requestdict['params']] )
+        replydict               = {
+            "jsonrpc":          requestdict['jsonrpc'],
+            "id":               requestdict['id'],
+            "result":           result,
+            "error":            None,
             }
-        svr.send_multipart( [sessid] + [json.dumps( replydict )],
+        svr.send_multipart( [sessid] + [json.dumps( replydict,
+                                                    **zmqjsonrpc.JSON_OPTIONS)],
                             prefix=prefix )
-    svrthr		= threading.Thread( target=svrfun )
+
+    svrthr                      = threading.Thread( target=svrfun )
     svrthr.start()
 
-    pxy			= zmqrpc.ServiceProxy(
-        socket=zmqrpc.session_socket( cli, sessid=["1"] ), name="boo" )
-    result 		= pxy.foo( 1, 2, 3 )
-
-
+    pxy                         = zmqjsonrpc.ServiceProxy(
+                                      socket=zmqjsonrpc.session_socket(
+                                          cli, sessid=["1"] ),
+                                      serviceName="boo" )
+    result                      = pxy.foo( 1, 2, 3 )
     assert result == "1, 2, 3"
+    del pxy
 
     svrthr.join()
-    del pxy
     svr.close()
     svrctx.term()
     cli.close()
