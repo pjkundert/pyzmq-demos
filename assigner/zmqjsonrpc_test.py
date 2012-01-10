319d4866cd45905a5410cc156bfbdbd234ba7f02
Perry Kundert <perry@kundert.ca>
2012-01-10 14:10:20 -0700

    Tidy simple 0MQ JSON-RPC proxy/server test

--- assigner/zmqjsonrpc_test.py
+++ assigner/zmqjsonrpc_test.py
@@ -1,27 +1,33 @@
 import zmq
 import zmqjsonrpc
+import zhelpers
 import json
 import threading
 
 def test_zmqrpc_simulate_server():
     clictx                      = zmq.Context()
-    cli                         = clictx.socket( zmq.XREQ )
+    cli                         = clictx.socket( zmq.REQ )
     cli.connect( 'tcp://localhost:11223' )
 
     svrctx                      = zmq.Context()
     svr                         = svrctx.socket( zmq.XREP )
     svr.bind( 'tcp://*:11223' )
 
-    # Simulate the Server side of the 0MQ JSON-RPC request
+    # Simulate the Server side of the 0MQ JSON-RPC request, receiving
+    # and processing the raw 0MQ request.
     def svrfun():
         request         	= svr.recv_multipart()
-        assert len( request ) == 4
-        assert request[1].lower() == "1"     # Session ID
-        assert request[2].lower() == zmqjsonrpc.CONTENT_TYPE
-        prefix			= request[:1]
-        sessid                  = request[1]
+        print "Server rx. [%s]" % (
+            ", ".join( [ zhelpers.format_part( m )
+                         for m in request ] ))
+        assert len( request ) == 5
+        assert request[1] == ""	   	     # Route prefix separator
+        assert request[2].lower() == "1"     # Session ID
+        assert request[3].lower() == zmqjsonrpc.CONTENT_TYPE
+        prefix			= request[:2]
+        sessid                  = request[2:3]
 
-        requestdict             = json.loads( request[3] )
+        requestdict             = json.loads( request[4] )
         assert requestdict['method'] == "boo.foo"
         assert len( requestdict['params'] ) == 3
 
@@ -33,9 +39,13 @@ def test_zmqrpc_simulate_server():
             "result":           result,
             "error":            None,
             }
-        svr.send_multipart( [sessid] + [json.dumps( replydict,
-                                                    **zmqjsonrpc.JSON_OPTIONS)],
-                            prefix=prefix )
+        reply		       	= [json.dumps( replydict,
+                                               **zmqjsonrpc.JSON_OPTIONS)]
+        print "Server tx. [%s]" % (
+            ", ".join( [ zhelpers.format_part( m )
+                         for m in prefix + sessid + reply ] ))
+            
+        svr.send_multipart( sessid + reply, prefix=prefix )
 
     svrthr                      = threading.Thread( target=svrfun )
     svrthr.start()
